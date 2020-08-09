914e0a3f427378e17fe54e866c119ffdc785d578
Van Kichline <vkichline@hotmail.com>
2020-08-09 09:04:17 -0700

    colors in calibration

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,16 @@ void draw_reading(JF_Reading& data) {
 }
 
 
-void draw_calibration(JF_CalDat& data) {
+void draw_calibration(JF_CalIn& data) {
+  M5.Lcd.setTextColor(data.xy_count < CALIBRATION_SAMPLES ? RED : GREEN, BLACK);
   String str = String(data.min_x) + "/" + String(data.max_x);
   M5.Lcd.drawString(str, 100,  65, 4);
   str = String(data.min_y) + "/" + String(data.max_y);
   M5.Lcd.drawString(str, 100, 115, 4);
+  M5.Lcd.setTextColor(data.center_count < CALIBRATION_SAMPLES ? RED : GREEN, BLACK);
   str = String(data.x_center / data.center_count) + "/" + String(data.y_center / data.center_count);
   M5.Lcd.drawString(str, 100, 165, 4);
+  M5.Lcd.setTextColor(WHITE, BLACK);
 }
 
 
@@ -63,11 +66,13 @@ void loop() {
   if(joy.is_calibrating()) {
     if(joy.calibrate()) {
       // Calibration is complete, is_calibrating will be false
+      draw_calibration(joy.get_calibration_info());
+      delay(500);   // to make sure the green numbers show up for both
       clear_calibration_display();
       M5.Lcd.drawCentreString("Calibrate", 60, 220, 2);
     }
     else {
-      draw_calibration(joy.get_calibration_data());
+      draw_calibration(joy.get_calibration_info());
     }
   }
   else if(joy.read(reading)) {
